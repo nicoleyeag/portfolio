d8450b94b6633e6399fbb29917b78a90bd29ff38
Nicole Yeager <nyeager95@gmail.com>
2025-04-04 16:18:51 -0600

    nav buttons are working

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav.js';
 import HomeButton from './components/HomeButton.js';
+import AboutMeButton from './components/AboutMeButton.js'
+import ProjectsButton from './components/ProjectsButton.js';
+import HireMeButton from './components/HireMeButton.js';
+import GitHubButton from './components/GitHubButton.js';
+import LinkedInButton from './components/LinkedInButton.js';
 
 const App = () => {
   return <>
    <Nav />
-   <HomeButton/>;
+   <HomeButton/>
+   <AboutMeButton/>
+   <ProjectsButton/>
+   <HireMeButton/>
+   <GitHubButton/>
+   <LinkedInButton/>
   </>;
 };
 
