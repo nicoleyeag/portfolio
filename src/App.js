9bf382c980a0c18899168abf149fb0e21fa944b3
Nicole Yeager <nyeager95@gmail.com>
2025-04-04 15:02:13 -0600

    initial button working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Nav from './components/Nav.js';
+import HomeButton from './components/HomeButton.js';
 
 const App = () => {
-  return <div className="App">App
-
-  </div>
-  ;
+  return <>
+   <Nav />
+   <HomeButton/>;
+  </>;
 };
 
 export default App;
