e834915bb392bee62ea5f1fe03f79e39155660bf
Nicole Yeager <nyeager95@gmail.com>
2026-01-07 15:32:04 -0700

    about me page started

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,37 @@
 import React from "react";
 
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <main className="about">
+        <section className = "about">
+            <div className = "about">
+                <h1><span className="highlight">About Me 🌟</span></h1>
+                 <p>
+                I’m a <span className="bold">front-end software engineer</span> with a passion for building sleek,
+                engaging, and <span className="bold">intuitive digital experiences</span>. My background spans 
+                <span className="bold">engineering</span>, accounting, teaching, and <span className="bold">design</span>, giving me a unique 
+                mix  of analytical problem-solving and creative thinking that I 
+                bring into my work. 
+                </p>
+                <p>
+                I started my software engineering journey as a 
+                self-taught developer, fueled by curiosity and a love for tech. Over 
+                the years, I’ve worked on <span className="bold">full-stack applications</span>, <span className="bold">UX/UI design</span>, and 
+                <span className="bold"> QA automation</span>, constantly pushing myself to learn, build, and 
+                refine. Right now, I’m focused on leveling up my skills, sharing my 
+                work on <span className="bold">GitHub</span>, and expanding my professional network.
+                </p>
+                <p>
+                Outside of 
+                tech, I’m always <span className="bold">learning</span> and <span className="bold">creating</span>—whether it’s baking macarons, 
+                designing, painting, or picking up new skills just for fun. I also 
+                spend a lot of time swimming, biking, paddleboarding, and exploring 
+                the outdoors.
+            </p>
+            </div>
+        </section>
+    </main>
+  )
+
+  
 }
