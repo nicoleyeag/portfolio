ee22dfbdf6988c055c1b49560cdf524c68defd07
Nicole Yeager <nyeager95@gmail.com>
2026-01-08 17:03:44 -0700

    hamburger menu for mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/style.css";
 import StatefulButton from "./StatefulButton";
 import GitHubButton from "./GitHubButton";
 import LinkedInButton from "./LinkedInButton";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <div className="nav-left">
-      <StatefulButton label="home" to="/" sparkleColor="red" />
-      <StatefulButton label="about me" to="/about" sparkleColor="orange" />
-      <StatefulButton label="projects" to="/projects" sparkleColor="yellow" />
-      <StatefulButton label="hire me!" to="/hire" sparkleColor="green" />
+      {/* Hamburger (CSS will hide this on desktop) */}
+      <button
+        type="button"
+        className="hamburger"
+        aria-label="Toggle navigation menu"
+        aria-expanded={menuOpen}
+        onClick={() => setMenuOpen((v) => !v)}
+      >
+        <span />
+        <span />
+        <span />
+      </button>
+
+      <div className={`nav-left ${menuOpen ? "is-open" : ""}`}>
+        <StatefulButton label="home" to="/" sparkleColor="red" />
+        <StatefulButton label="about me" to="/about" sparkleColor="orange" />
+        <StatefulButton label="projects" to="/projects" sparkleColor="yellow" />
+        <StatefulButton label="hire me!" to="/hire" sparkleColor="green" />
       </div>
 
       <div className="nav-right">
-      <GitHubButton />
-      <LinkedInButton />
+        <GitHubButton />
+        <LinkedInButton />
       </div>
     </nav>
   );
